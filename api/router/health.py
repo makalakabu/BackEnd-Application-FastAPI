dc601bfff7bf881add60393975ba1dfe262c59ac
makalakabu <marcelldavid11@gmail.com>
2026-01-27 17:15:41 +0000

    Add Health Endpoint

--- api/router/health.py
+++ api/router/health.py
@@ -6,12 +6,12 @@ from api.deps import get_db
 
 router = APIRouter(tags=["health"])
 
-@router.get("/health")
+@router.get("/health", status_code=200)
 def health(db: Session = Depends(get_db)) -> dict:
     try:
         db.execute(text("SELECT 1"))
     except SQLAlchemyError:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Database Unavailable")
     
-    return {"status" :"ok", "database": "ok"}
+    return {"status" :"ok", "db": "ok"}
     
